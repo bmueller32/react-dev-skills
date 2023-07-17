cd5628e5184f010b8ab8ca7ff8654f4f5e608a59
Brent <brentmueller32@gmail.com>
2023-07-17 09:29:06 -0700

    setup skill list item ul

---
+++ src/SkillList.jsx
@@ -0,0 +1,13 @@
+
+export default function SkillList(){
+    //app logic goes here
+
+    //return UI as JSX
+    return (
+        <ul>
+            <SkillListItem/>
+            <SkillListItem/>
+            <SkillListItem/>
+        </ul>
+    )
+}
